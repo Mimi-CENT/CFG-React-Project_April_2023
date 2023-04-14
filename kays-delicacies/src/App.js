090e7743e3ef7e09c805b4032d3ba16184eb5d21
Mimi-CENT <107249650+Mimi-CENT@users.noreply.github.com>
2023-04-14 22:40:49 +0100

    Created components and added routing/navigation

--- kays-delicacies/src/App.js
+++ kays-delicacies/src/App.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import NavBar from './Components/NavBar';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+
+import Home from "./Components/Home";
+import Menu from "./Components/Menu";
+import Reviews from "./Components/Reviews";
+import ContactUs from "./Components/ContactUs";
+import ErrorPage from './Components/ErrorPage';
 
 function App() {
   return (
-    <div className="App">
-     
-    </div>
+    <Router>
+    <NavBar/>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/menu" element={<Menu />}/>
+      <Route path="/reviews" element={<Reviews />}/>
+      <Route path="/contactus" element={<ContactUs />}/>
+      {/* last route to add = error */}
+      <Route path="*" element={<ErrorPage />}/>
+    </Routes>
+    {/* <Footer/> */}
+  </Router>
   );
 }
 
