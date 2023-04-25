e447ecfce2d932cac6639e7dc23f2056cadf6994
Mimi-CENT <107249650+Mimi-CENT@users.noreply.github.com>
2023-04-25 22:38:23 +0100

    Modified Error page

---
+++ kays-delicacies/src/Components/ErrorPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../Styles/Home.css';
+
+
+const ErrorPage = () => {
+  return (
+    <div>
+      <h1 className='error'>Error! This page doesn't exist </h1>
+    </div>
+  )
+}
+
+export default ErrorPage
